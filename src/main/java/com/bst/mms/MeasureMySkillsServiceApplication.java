baf637a37cfc2cbf1a21053aa196e99fb02bd8a3
Somanadha Satya Dev Bulusu <soma_satya@yahoo.com>
2024-06-24 12:29:44 +0530

    Initial Commit

---
+++ src/main/java/com/bst/mms/MeasureMySkillsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bst.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureMySkillsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasureMySkillsServiceApplication.class, args);
+		System.out.println("Tomcat Running on port 8080 for Measure My Skills Application...");
+	}
+}
